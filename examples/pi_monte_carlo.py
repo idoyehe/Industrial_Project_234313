4e04eb32098ac6f94a2f814e190afd9d7feeddbb
idoye <idoye@campus.technion.ac.il>
2018-11-11 19:20:01 +0200

    edit pi monte carlo

--- examples/pi_monte_carlo.py
+++ examples/pi_monte_carlo.py
@@ -8,25 +8,40 @@ will launch the reduce function.
 """
 import pywren_ibm_cloud as pywren
 from random import random
-iterdata = [False] * 10
+
+ACTIONS = 55
+PER_ACTION = 1009000
+
+iterdata = [False] * ACTIONS
 
 
 def my_map_function(curr):
-    x = random() - 0.5
-    y = random() - 0.5
-    return (x ** 2) + (y ** 2) <= 0.25
+    points = []
+    for i in range(PER_ACTION):
+        x = random()
+        y = random()
+        points.append((x ** 2) + (y ** 2) <= 1)
+    return points
 
 
 def my_reduce_function(results):
     in_circle = 0
+    num_samples = 0
     for map_result in results:
-        in_circle = in_circle + int(map_result)
-    return float(4 * (in_circle / len(results)))
+        for res in map_result:
+            in_circle = in_circle + int(res)
+            num_samples += 1
+    return float(4 * (in_circle / num_samples))
+
 
 """
 Set 'reducer_wait_local=False' to launch the reducer and wait for
 the results remotely.
 """
+# for i in range(len(iterdata)):
+#     iterdata[i] = my_map_function(iterdata[i])
+# print(my_reduce_function(iterdata))
+
 pw = pywren.ibm_cf_executor()
 pw.map_reduce(my_map_function, iterdata, my_reduce_function, reducer_wait_local=False)
 print(pw.get_result())
