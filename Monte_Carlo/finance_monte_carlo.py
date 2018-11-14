5f73c4f6e48f80a13648fe2afe376f4b509d2ee8
idoye <idoye@campus.technion.ac.il>
2018-11-14 21:04:25 +0200

    change imports in the finance monte carlo adding monte finance monte carlo url

--- Monte_Carlo/finance_monte_carlo.py
+++ Monte_Carlo/finance_monte_carlo.py
@@ -1,9 +1,8 @@
 import pywren_ibm_cloud as pywren
-from random import random
 from time import time
-from math import exp
+from numpy import median, exp, random
+import matplotlib.pyplot as plt
 from scipy.stats import norm
-from numpy import median
 
 
 class StockData:
@@ -17,22 +16,23 @@ class StockData:
     def prediction(self):
         predicts_est = [self.last_value]
         for predict in range(1, self.days2predict + 1):
-            rand = random()
+            rand = random.rand()
             pow_r = norm.ppf(rand)
             predicts_est.append(predicts_est[predict - 1] * exp(self.drift + (self.std_dev * pow_r)))
         return predicts_est
 
 
-ACTIONS = 2000
+ACTIONS = 1000
 TOTAL = ACTIONS
-print("Total Random Samples is: " + str(TOTAL))
+print("Total Prediction: " + str(TOTAL))
 
-gold = StockData(name="GOLD", drift=0.000142559, std_dev=0.010561899, last_value=1296.5, days2predict=219)
-mlnx = StockData(name="Mellanox", drift=0.000581742829, std_dev=0.029879238, last_value=64.7, days2predict=219)
-ibm = StockData(name="IBM", drift=0.000091967236, std_dev=0.012404562, last_value=153.42, days2predict=219)
-nvda = StockData(name="Nvdia", drift=0.000936809, std_dev=0.027145343, last_value=193.5, days2predict=219)
+gold = StockData(name="GOLD", drift=0.000142559, std_dev=0.010561899, last_value=1296.5, days2predict=1095)
+mlnx = StockData(name="Mellanox", drift=0.000581742829, std_dev=0.029879238, last_value=64.7, days2predict=1095)
+ibm = StockData(name="IBM", drift=0.000091967236, std_dev=0.012404562, last_value=153.42, days2predict=1095)
+nvda = StockData(name="Nvdia", drift=0.000936809, std_dev=0.027145343, last_value=193.5, days2predict=1095)
 
-stock = gold
+stock = ibm
+print("Current Stock: " + stock.name)
 
 iterdata = [[]] * ACTIONS
 
@@ -50,21 +50,24 @@ Set 'reducer_wait_local=False' to launch the reducer and wait for
 the results remotely.
 """
 
-# FLAG = "LOCAL"
-FLAG = "CLOUD"
+FLAG = "LOCAL"
+# FLAG = "CLOUD"
 if FLAG == "LOCAL":
     start_time = time()
     for i in range(ACTIONS):
         iterdata[i] = my_map_function()
-    print("Amortized Values are: ")
-    print(my_reduce_function(iterdata))
+    result_list = my_reduce_function(iterdata)
 else:
     start_time = time()
     pw = pywren.ibm_cf_executor()
     pw.map_reduce(my_map_function, iterdata, my_reduce_function, reducer_wait_local=False)
     result_list = pw.get_result()
-    print("Amortized Values are: ")
-    print(result_list)
+
 elapsed = time()
 
+print("Stock values prediction: ")
+print(result_list)
+
 print("\nDuration: " + str(elapsed - start_time) + " Sec")
+plt.plot([x for x in range(stock.days2predict + 1)], result_list)
+plt.show()
