7e66c01271639daf5060bc7e3b8cda33b2a25555
idoye <idoye@campus.technion.ac.il>
2018-11-28 20:55:28 +0200

    update pywren examples

--- examples/pywrentest.py
+++ examples/pywrentest.py
@@ -7,8 +7,7 @@ import pywren_ibm_cloud as pywren
 def my_function(x):
     return x + 7
 
-
 if __name__ == '__main__':
     pw = pywren.ibm_cf_executor()
     pw.call_async(my_function, 3)
-    print(pw.get_result())
+    print (pw.get_result())
