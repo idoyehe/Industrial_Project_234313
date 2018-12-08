0aacd234ebc7d6c9abd389788b705b61c50300e6
idoye <idoye@campus.technion.ac.il>
2018-12-09 00:29:57 +0200

    operations time measurements

--- fasttext_pywren/amazon_real_reviews_model/fasttext_amazon_review.py
+++ fasttext_pywren/amazon_real_reviews_model/fasttext_amazon_review.py
@@ -6,17 +6,17 @@ train_file_path = "./amazon_real_reviews_model/database/all_reviews.train"
 bin_file_path = "./amazon_real_reviews_model/database/all_reviews.bin"
 test_file_path = "./amazon_real_reviews_model/database/all_reviews.test"
 
-# start_time = time()
-# fasttext_model = train_supervised(train_file_path, lr=1.0, wordNgrams=10, loss="hs")
-# elapsed = time()
-# duration = elapsed - start_time
-# print("\nDuration to BUILD the model: " + str(duration) + " Sec")
-#
-# start_time = time()
-# fasttext_model.save_model(bin_file_path)
-# elapsed = time()
-# duration = elapsed - start_time
-# print("\nDuration to SAVE the model: " + str(duration) + " Sec")
+start_time = time()
+fasttext_model = train_supervised(train_file_path, lr=1.0, wordNgrams=5, loss="hs")
+elapsed = time()
+duration = elapsed - start_time
+print("\nDuration to BUILD the model: " + str(duration) + " Sec")
+
+start_time = time()
+fasttext_model.save_model(bin_file_path)
+elapsed = time()
+duration = elapsed - start_time
+print("\nDuration to SAVE the model: " + str(duration) + " Sec")
 
 start_time = time()
 fasttext_model = load_model(bin_file_path)
