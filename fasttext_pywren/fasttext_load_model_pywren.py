6368d7a21f15a1c994ec8c2d632241d1f43e03e5
idoye <idoye@campus.technion.ac.il>
2018-12-09 23:01:16 +0200

    first fasttext code

--- fasttext_pywren/fasttext_load_model_pywren.py
+++ fasttext_pywren/fasttext_load_model_pywren.py
@@ -5,6 +5,7 @@ import sys
 import yaml
 import fastText as fstxt
 import smart_open
+import fasttext_pybind
 
 config_filename = "~/.pywren_config"
 
@@ -16,23 +17,20 @@ try:
 except Exception as e:
     print("can't open config file".format(e))
     sys.exit()
-cos_endpoint = config['ibm_cos']['endpoint']
+cos_endpoint = "s3.us-east.objectstorage.service.networklayer.com"
 access_key = config['ibm_cos']['access_key']
 secret_key = config['ibm_cos']['secret_key']
-cos_session = ibm_boto3.session.Session(aws_access_key_id=access_key, aws_secret_access_key=secret_key)
-
-storage_bucket = "fasttext-models"
-model_filepath = 's3://' + storage_bucket + '/all_reviews.bin'
 
 
-def my_function(x):
-    import fasttext_pybind as fastxt_pyb
-    fastText_model = fstxt.load_model(smart_open.smart_open(model_filepath,
-                                                            host=cos_endpoint,
-                                                            s3_session=cos_session))
-    return fastText_model.get_labels(include_freq=False)
+def my_function():
+    cos_session = ibm_boto3.session.Session(aws_access_key_id=access_key, aws_secret_access_key=secret_key, region_name='us=east')
+    storage_bucket = "fasttext-models"
+    model_filepath = 's3://' + storage_bucket + '/all_reviews.bin'
+    # fastText_model = fstxt.load_model(
+    smart_open.smart_open(model_filepath, host=cos_endpoint, s3_session=cos_session)
+    # return fastText_model.get_labels(include_freq=False)
 
-
-pw = pywren.ibm_cf_executor()
-pw.call_async(my_function, 3)
-print(pw.get_result())
+my_function()
+# pw = pywren.ibm_cf_executor()
+# pw.call_async(my_function, fastText_model)
+# print(pw.get_result())
