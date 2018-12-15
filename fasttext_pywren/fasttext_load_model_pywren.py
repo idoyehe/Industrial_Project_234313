c226b0fe7545d7cf0a14db6e6bb4846dcbd540f0
idoye <idoye@campus.technion.ac.il>
2018-12-15 15:46:26 +0200

    adding pywren code to predict fasttext

--- fasttext_pywren/fasttext_load_model_pywren.py
+++ fasttext_pywren/fasttext_load_model_pywren.py
@@ -1,14 +1,39 @@
-import fastText as fstxt
+import fastText as fstTxt
 import pywren_ibm_cloud as pywren
 
+ag_news_model = "/fasttext/models/ag_news.ftz"  # from docker
+amazon_review_model = "/fasttext/models/amazon_review_polarity.ftz"  # from docker
+dbpedia_model = "/fasttext/models/dbpedia.ftz"  # from docker
+sogou_news_model = "/fasttext/models/sogou_news.ftz"  # from docker
+yelp_review_model = "/fasttext/models/yelp_review_full.ftz"  # from docker
 
-model_path = "/fasttext/models/yelp_review_full.ftz"
+bucketname = 'fasttext-predict-bucket'
 
-def my_function(x):
-    fasttext_model = fstxt.load_model(model_path)
-    return fasttext_model.predict("IDO is Best men"), fasttext_model.predict("IDO is Worst men")
+files_names = ['ag_news_predict.txt',
+               'dbpedia_predict.txt',
+               'sogou_predict.txt',
+               'yelp_review_predict.txt']
 
+files_to_predict = list(map(lambda s: bucketname + '/' + s, files_names))
+
+
+def map_fasttext_function(key, data_stream):
+    print('I am processing the object {}'.format(key))
+    fasttext_model = fstTxt.load_model(ag_news_model)
+
+    data = data_stream.read()
+    for line in data.splitlines():
+        fasttext_model.predict(line)
+
+    return True
+
+
+def my_reduce_function(results):
+    return True
+
+
+chunk_size = 4 * 1024 ** 2  # 4MB
 
 pw = pywren.ibm_cf_executor(runtime="fasttext-exists-models")
-pw.call_async(my_function, 3)
+pw.map_reduce(map_fasttext_function, files_to_predict[0], my_reduce_function, chunk_size)
 print(pw.get_result())
