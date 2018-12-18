15599c9aec596e100dd154bb198cd72fb42516d5
idoye <idoye@campus.technion.ac.il>
2018-12-18 18:40:02 +0200

    environment update

--- fasttext_pywren/fasttext_load_model_pywren.py
+++ fasttext_pywren/fasttext_load_model_pywren.py
@@ -2,6 +2,7 @@ from time import time
 import fastText as fstTxt
 import pywren_ibm_cloud as pywren
 from pickle import dump
+from sys import getsizeof
 
 ag_news_model = "/fasttext/models/ag_news.ftz"  # from docker
 amazon_review_model = "/fasttext/models/amazon_review_polarity.ftz"  # from docker
@@ -13,7 +14,7 @@ bucketname = 'fasttext-predict-bucket'
 
 files_names = ['ag_news_predict.txt',  # 27.6 MB ~ 40 sec
                'dbpedia_predict.txt',  # 164.8 MB ~ 158.247 sec when chunk size is 4MB
-               'sogou_predict.txt',  # 1.2 GB ~ 90 sec when chunk size is 16MB
+               'sogou_predict.txt',  # 1.2 GB ~ 90 sec when chunk size is 16MB ~ 144 sec
                'yelp_review_predict.txt']  # 456.2 MB ~ 247 MB
 
 files_to_predict = list(map(lambda s: bucketname + '/' + s, files_names))
@@ -31,12 +32,12 @@ def map_fasttext_function(key, data_stream):
     return my_list
 
 
-def reduce_function(results, futures):
-    my_result = []
+def reduce_function(results):
+    my_result = list()
 
     for my_list in results:
         my_result.extend(my_list)
-    return {"futures": futures, "results": my_result}
+    return {"futures": None, "results": getsizeof(my_result)}
 
 
 chunk_size = 8 * 1024 ** 2  # 4MB
@@ -52,8 +53,8 @@ if futures is not None:
     invoke_statuses = [f.invoke_status for f in futures]
     res = {'run_statuses': run_statuses, 'invoke_statuses': invoke_statuses}
     dump(res, open('../InvocationsGraphsFiles/statuses.pickle', 'wb'), -1)
-pw.clean()
 
 end = time()
 duration = end - start
 print("\nDuration: " + str(duration) + " Sec")
+print("\nResult Size: " + str(result_object['results']) + " Bytes")
