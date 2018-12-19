dfe5743878b1bce69a11432995ab468b33936da5
idoye <idoye@campus.technion.ac.il>
2018-12-19 17:48:43 +0200

    no code change

--- MonteCarlo/finance_monte_carlo.py
+++ MonteCarlo/finance_monte_carlo.py
@@ -27,13 +27,14 @@ class StockData:
 
 
 ibm_10 = StockData(title="IBM Based last 10 years", drift=0.0000579602177315899, std_dev=0.0119319087951656, last_value=116.49)
-ibm_2016_2017_2018 = StockData(title="IBM Based 2016, 2017, 2018 (end of October)", drift=-0.000295076840817479, std_dev=0.0118636061422374,
-                  last_value=116.49)
+ibm_2016_2017_2018 = StockData(title="IBM Based 2016, 2017, 2018 (end of October)", drift=-0.000295076840817479,
+                               std_dev=0.0118636061422374, last_value=116.49)
 ibm_2014_2015_2016 = StockData(title="IBM 2014, 2015, 2016", drift=-0.00022513546014255100, std_dev=0.0121678341323272,
                                last_value=166.44)
 
 intel_10 = StockData(title="Intel Based last 10 years", drift=0.000359910620036371, std_dev=0.0155512505615464, last_value=48.29)
-intel_2016_2017_2018 = StockData(title="Intel Based 2016, 2017, 2018 (end of October)", drift=0.00038623298498201, std_dev=0.0148876589619472, last_value=48.29)
+intel_2016_2017_2018 = StockData(title="Intel Based 2016, 2017, 2018 (end of October)", drift=0.00038623298498201,
+                                 std_dev=0.0148876589619472, last_value=48.29)
 intel_2014_2015_2016 = StockData(title="Intel 2014, 2015, 2016", drift=0.00036084284127726200, std_dev=0.0143579745383959,
                                  last_value=36.79)
 
