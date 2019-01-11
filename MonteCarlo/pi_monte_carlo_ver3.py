813812bbb415cf5547f2b5809d01433d6112b2db
idoye <idoye@campus.technion.ac.il>
2019-01-11 11:34:35 +0200

    update pi for dist

--- MonteCarlo/pi_monte_carlo_ver3.py
+++ MonteCarlo/pi_monte_carlo_ver3.py
@@ -1,34 +1,48 @@
 from random import random
 from ExecuterWrapper.executorWrapper import ExecutorWrap, Location
+import pywren_ibm_cloud as pywren
 
 exe_location = Location.PYWREN
 
-ACTIONS = 1000
-PER_ACTION = 10000000
-print("Total points is: " + str(PER_ACTION * ACTIONS))
-
-iterdata = [0] * ACTIONS
+MAP_INSTANCES = 10
 
 
-def my_map_function(curr):
-    total_in_circle = 0
-    for i in range(PER_ACTION):
+class EstimatePI:
+    randomize_per_map = 10000000
+
+    def __init__(self):
+        self.total_randomize_points = MAP_INSTANCES * self.randomize_per_map
+
+    def __str__(self):
+        return "Total Randomize Points: {:,}".format(self.randomize_per_map)
+
+    @staticmethod
+    def predicate():
         x = random()
         y = random()
-        total_in_circle += ((x ** 2) + (y ** 2) <= 1)
-    return float(total_in_circle / PER_ACTION)
+        return (x ** 2) + (y ** 2) <= 1
+
+    def randomize_points(self, iter):
+        in_circle = 0
+        for _ in range(self.randomize_per_map):
+            in_circle += self.predicate()
+        return float(in_circle / self.randomize_per_map)
+
+    def process_in_circle_points(self, results, futures):
+        in_circle_percent = 0
+        for map_result in results:
+            in_circle_percent += map_result
+        run_statuses = [f.run_status for f in futures]
+        invoke_statuses = [f.invoke_status for f in futures]
+        estimate_PI = float(4 * (in_circle_percent / MAP_INSTANCES))
+        return {"run_statuses": run_statuses, "invoke_statuses": invoke_statuses, "results": estimate_PI}
 
 
-def my_reduce_function(results, futures):
-    sumPI = 0
-    for map_result in results:
-        sumPI += map_result
-    run_statuses = [f.run_status for f in futures]
-    invoke_statuses = [f.invoke_status for f in futures]
-    return {"run_statuses": run_statuses, "invoke_statuses": invoke_statuses, "results": float(4 * (sumPI / ACTIONS))}
-
-
-executor = ExecutorWrap(ACTIONS, "Pi_monte_carlo_" + str(PER_ACTION * ACTIONS))
-result_obj = executor.map_reduce_execution(my_map_function, iterdata, my_reduce_function)
+est_pi = EstimatePI()
+iterdata = [0] * MAP_INSTANCES
+executor = ExecutorWrap(MAP_INSTANCES, "Pi_monte_carlo_" + str(est_pi))
+executor.set_location(exe_location)
+result_obj = executor.map_reduce_execution(est_pi.randomize_points, iterdata, est_pi.process_in_circle_points)
+print(str(est_pi))
 print("Amortized PI is: ")
 print(result_obj)
