bce2d844827cca3c73a68b62ae031b4ce15b689b
idoye <idoye@campus.technion.ac.il>
2019-03-22 10:31:49 +0200

    update local evaluations

--- fasttext_pywren/HyperParametersEvaluate/fastTextHyperparametersEvaluate_local.py
+++ fasttext_pywren/HyperParametersEvaluate/fastTextHyperparametersEvaluate_local.py
@@ -2,48 +2,52 @@ import fastText as fstTxt
 from fasttext_pywren.HyperParametersEvaluate.localGenericHyperparameterEvaluate import *
 import csv
 
-models_names = {"dbpedia": "../FastTextModels/dbpedia/dbpedia.train",
-                "yelp": "../FastTextModels/yelp_review_full/yelp_review_full.train"}
+# from fasttext_pywren.HyperParametersEvaluate.train_file_partitioner import KFoldCrossPartitioner
+# models_names = {"ag_news": "../FastTextModels/ag_news/ag_news.train",
+#                 "dbpedia": "../FastTextModels/dbpedia/dbpedia.train",
+#                 "yelp": "../FastTextModels/yelp_review_full/yelp_review_full.train"}
 
+# ag_news = KFoldCrossPartitioner(5,
+#                                 models_names["ag_news"],
+#                                 "./folds_ag_news/",
+#                                 "ag_news")
+# ag_news.k_fold_cross_partitioner()
+#
+# dbpedia_folds = KFoldCrossPartitioner(5,
+#                                       models_names["dbpedia"],
+#                                       "./folds_dbpedia/",
+#                                       "dbpedia")
+# dbpedia_folds.k_fold_cross_partitioner()
+#
+# yelp_folds = KFoldCrossPartitioner(5,
+#                                       models_names["yelp"],
+#                                       "./folds_yelp/",
+#                                       "yelp")
+#
+# yelp_folds.k_fold_cross_partitioner()
 
 def fastText_evaluate(train_path, test_path, hyperparameters_set):
+    start = time()
     to_valid_model = fstTxt.train_supervised(train_path, **hyperparameters_set)
     result = to_valid_model.test(test_path)
-    return {"precision": result[1], "recall": result[2]}
-
-
+    end = time()
+    return {"precision": result[1], "recall": result[2], "cpu_time": end - start}
 
 
 """experiments 1"""
 if __name__ == '__main__':
-    results = []
-    for i in range(5):
-        lkf = LocalKFoldCrossValidation(5, models_names['dbpedia'], ("precision", "recall"), fastText_evaluate)
-        results.append(lkf.hyperparameters_kfc())
+    for model in ["ag_news", "dbpedia", "yelp"]:
+        results = []
+        for i in range(10):
+            lkf = LocalKFoldCrossValidation(5, model, ("precision", "recall", "cpu_time"), fastText_evaluate)
+            results.append(lkf.hyperparameters_kfc_parallel())
 
-    parsed_results = []
-    for inner_res in results:
-        completion_time = {"completion_time": inner_res["completion_time"]}
-        parsed_results.append({**inner_res["results"][0], **completion_time})
+        parsed_results = []
+        for inner_res in results:
+            parsed_results.append({**inner_res["results"][0], **{'total_completion_time': inner_res['total_completion_time']}})
 
-    f = open("./marc_experiments/exp_1_dbpedia.csv", 'w')
-    writer = csv.DictWriter(f, fieldnames=["precision", "recall", "validation_completion_time","completion_time"])
-    writer.writeheader()
-    writer.writerows(parsed_results)
-    f.close()
-
-    results = []
-    for i in range(5):
-        lkf = LocalKFoldCrossValidation(5, models_names['yelp'], ("precision", "recall"), fastText_evaluate)
-        results.append(lkf.hyperparameters_kfc())
-
-    parsed_results = []
-    for inner_res in results:
-        completion_time = {"completion_time": inner_res["completion_time"]}
-        parsed_results.append({**inner_res["results"][0], **completion_time})
-
-    f = open("./marc_experiments/exp_1_yelp.csv", 'w')
-    writer = csv.DictWriter(f, fieldnames=["precision", "recall", "validation_completion_time","completion_time"])
-    writer.writeheader()
-    writer.writerows(parsed_results)
-    f.close()
+        f = open("./marc_experiments/exp_1_" + model + ".csv", 'w')
+        writer = csv.DictWriter(f, fieldnames=["precision", "recall", "cpu_time", "total_completion_time"])
+        writer.writeheader()
+        writer.writerows(parsed_results)
+        f.close()
--- fasttext_pywren/HyperParametersEvaluate/localGenericHyperparameterEvaluate.py
+++ fasttext_pywren/HyperParametersEvaluate/localGenericHyperparameterEvaluate.py
@@ -1,23 +1,5 @@
-from sklearn.model_selection import KFold
-from os import mkdir
-from shutil import rmtree
 import billiard as multiprocessing
 from time import time
-
-
-def _createValidationFiles(_labeled_data: list, _train_index: list, _test_index: list, _train_file_name: str, _test_file_name: str,
-                           _path: str):
-    mkdir(_path)
-    train_path = _path + _train_file_name
-    test_path = _path + _test_file_name
-    train_lines = [_labeled_data[index] + "\n" for index in _train_index]
-    test_lines = [_labeled_data[index] + "\n" for index in _test_index]
-    train_file = open(train_path, 'w')
-    test_file = open(test_path, 'w')
-    train_file.writelines(train_lines)
-    test_file.writelines(test_lines)
-    test_file.close()
-    train_file.close()
 
 
 def _reducer_average_validator(evaluate_keys, results):
@@ -35,120 +17,65 @@ def _reducer_average_validator(evaluate_keys, results):
 
 
 def _evaluate_single_hyperparameters_parallel(k_value,
-                                              folders_prefix,
-                                              train_file_name,
-                                              test_file_name,
+                                              folder_path,
+                                              model_name,
                                               evaluate_function,
                                               evaluate_keys,
                                               hyperparameters_set={}):
-    start_time = time()
     args_list = []
     for eval_index in range(k_value):
-        path: str = folders_prefix + str(eval_index) + "/"
-        train_path: str = path + train_file_name
-        test_path: str = path + test_file_name
+        path: str = folder_path
+        train_path: str = path + model_name + "_fold_" + str(eval_index) + ".train"
+        test_path: str = path + model_name + "_fold_" + str(eval_index) + ".test"
         args_list.append((train_path, test_path, hyperparameters_set))
 
     p = multiprocessing.Pool(k_value)
     results = p.starmap(func=evaluate_function, iterable=args_list)
     results = _reducer_average_validator(evaluate_keys, results)
-    results['validation_completion_time'] = time() - start_time
     return results
 
 
 class LocalKFoldCrossValidation(object):
-    def __init__(self, k_value: int, train_file: str, evaluate_keys, evaluate_function):
+    def __init__(self, k_value: int, model_name: str, evaluate_keys, evaluate_function):
         self.k_value: int = k_value
-        self.train_file: str = train_file
+        self.model_name: str = model_name
         self.evaluate_function = evaluate_function
         self.evaluate_keys = evaluate_keys
-
-        self.folders_prefix = "./fold_"
-        self.train_file_name = "database.train"
-        self.test_file_name = "database.test"
-
-    def __k_partitioner_parallel(self):
-        opened_train_file = open(self.train_file, 'r')
-        labeled_data = opened_train_file.read().splitlines()
-        kf = KFold(n_splits=self.k_value, shuffle=True)
-        splits = kf.split(labeled_data)
-        i = 0
-
-        args_list = []
-        for train_index, test_index in splits:
-            path: str = self.folders_prefix + str(i) + "/"
-            i += 1
-            args_list.append((labeled_data,
-                              train_index,
-                              test_index,
-                              self.train_file_name,
-                              self.test_file_name,
-                              path))
-        p = multiprocessing.Pool(self.k_value)
-        p.starmap(func=_createValidationFiles, iterable=args_list)
-        opened_train_file.close()
-
-    def __cleaner(self):
-        for i in range(self.k_value):
-            rmtree(self.folders_prefix + str(i), ignore_errors=True)
+        self.folder_path = "./folds_" + model_name + "/"
 
     def _evaluate_single_hyperparameters_serial(self, hyperparameters_set={}):
         start_time = time()
         results_list = []
         for eval_index in range(self.k_value):
-            path: str = self.folders_prefix + str(eval_index) + "/"
-            train_path: str = path + self.train_file_name
-            test_path: str = path + self.test_file_name
+            path: str = self.folder_path
+            train_path: str = path + self.model_name + "_fold_" + str(eval_index) + ".train"
+            test_path: str = path + self.model_name + "_fold_" + str(eval_index) + ".test"
             results_list.append(self.evaluate_function(train_path, test_path, hyperparameters_set))
 
         results = _reducer_average_validator(self.evaluate_keys, results_list)
         results['validation_completion_time'] = time() - start_time
         return results
 
-    def hyperparameters_kfc_parallel(self, hyperparameters_sets=[[{}]]):
-        self.__cleaner()  # make sure last session cleaned
+    def hyperparameters_kfc_serial(self, hyperparameters_sets=[[{}]]):
         start_time = time()
-        self.__k_partitioner_parallel()
+        results = []
+        for hyp_set in hyperparameters_sets:
+            results.append(self._evaluate_single_hyperparameters_serial(hyp_set[0]))
+        total_completion_time = time() - start_time
+        return {"results": results, "total_completion_time": total_completion_time}
+
+    def hyperparameters_kfc_parallel(self, hyperparameters_sets=[[{}]]):
+        start_time = time()
         args_list = []
         for hyp_set in hyperparameters_sets:
             args_list.append((self.k_value,
-                              self.folders_prefix,
-                              self.train_file_name,
-                              self.test_file_name,
+                              self.folder_path,
+                              self.model_name,
                               self.evaluate_function,
                               self.evaluate_keys,
                               hyp_set[0]))
 
         p = multiprocessing.Pool(len(hyperparameters_sets))
         results = p.starmap(func=_evaluate_single_hyperparameters_parallel, iterable=args_list)
-        completion_time = time() - start_time
-        self.__cleaner()  # make sure current session cleaned
-        return {"results": results, "completion_time": completion_time}
-
-    def hyperparameters_kfc_serial(self, hyperparameters_sets=[[{}]]):
-        self.__cleaner()  # make sure last session cleaned
-        start_time = time()
-        opened_train_file = open(self.train_file, 'r')
-        labeled_data = opened_train_file.read().splitlines()
-        kf = KFold(n_splits=self.k_value, shuffle=True)
-        splits = kf.split(labeled_data)
-
-        i = 0
-        for train_index, test_index in splits:
-            path: str = self.folders_prefix + str(i) + "/"
-            i += 1
-            _createValidationFiles(labeled_data,
-                                   train_index,
-                                   test_index,
-                                   self.train_file_name,
-                                   self.test_file_name,
-                                   path)
-
-        opened_train_file.close()
-
-        results = []
-        for hyp_set in hyperparameters_sets:
-            results.append(self._evaluate_single_hyperparameters_serial(hyp_set[0]))
-        completion_time = time() - start_time
-        self.__cleaner()  # make sure current session cleaned
-        return {"results": results, "completion_time": completion_time}
+        total_completion_time = time() - start_time
+        return {"results": results, "total_completion_time": total_completion_time}
