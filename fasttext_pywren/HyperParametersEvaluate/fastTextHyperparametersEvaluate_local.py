362f4c3898d1b8e0d96a68cc4b8d5b1fa8298a7f
idoye <idoye@campus.technion.ac.il>
2019-03-22 10:59:35 +0200

    finish local experiments code

--- fasttext_pywren/HyperParametersEvaluate/fastTextHyperparametersEvaluate_local.py
+++ fasttext_pywren/HyperParametersEvaluate/fastTextHyperparametersEvaluate_local.py
@@ -1,6 +1,8 @@
 import fastText as fstTxt
 from fasttext_pywren.HyperParametersEvaluate.localGenericHyperparameterEvaluate import *
+from fasttext_pywren.HyperParametersEvaluate.randomHyperparameters import random_search
 import csv
+
 
 # from fasttext_pywren.HyperParametersEvaluate.train_file_partitioner import KFoldCrossPartitioner
 # models_names = {"ag_news": "../FastTextModels/ag_news/ag_news.train",
@@ -34,20 +36,40 @@ def fastText_evaluate(train_path, test_path, hyperparameters_set):
     return {"precision": result[1], "recall": result[2], "cpu_time": end - start}
 
 
-"""experiments 1 default hyperparameters"""
 if __name__ == '__main__':
+    """experiments 1 default hyperparameters"""
+    # for model in ["ag_news", "dbpedia", "yelp"]:
+    #     results = []
+    #     for i in range(10):
+    #         lkf = LocalKFoldCrossValidation(5, model, ("precision", "recall", "cpu_time"), fastText_evaluate)
+    #         results.append(lkf.hyperparameters_kfc_parallel())
+    #
+    #     parsed_results = []
+    #     for iteration_result in results:
+    #         parsed_results.append({**iteration_result["results"][0], 'total_completion_time': iteration_result['total_completion_time']})
+    #
+    #     f = open("./marc_experiments/exp_1_" + model + ".csv", 'w')
+    #     writer = csv.DictWriter(f, fieldnames=["precision", "recall", "cpu_time", "total_completion_time"])
+    #     writer.writeheader()
+    #     writer.writerows(parsed_results)
+    #     f.close()
+
+    """experiments 2 random search hyperparameters"""
+    hyperparameters_sets = random_search(2)
     for model in ["ag_news", "dbpedia", "yelp"]:
         results = []
-        for i in range(10):
+        for i in range(2):
             lkf = LocalKFoldCrossValidation(5, model, ("precision", "recall", "cpu_time"), fastText_evaluate)
-            results.append(lkf.hyperparameters_kfc_parallel())
+            results.append(lkf.hyperparameters_kfc_parallel(hyperparameters_sets))
 
         parsed_results = []
-        for inner_res in results:
-            parsed_results.append({**inner_res["results"][0], **{'total_completion_time': inner_res['total_completion_time']}})
+        for iteration_result in results:
+            for set_index, set_res in enumerate(iteration_result["results"]):
+                parsed_results.append(
+                    {"set_index": set_index, **set_res, 'total_completion_time': iteration_result['total_completion_time']})
 
-        f = open("./marc_experiments/exp_1_" + model + ".csv", 'w')
-        writer = csv.DictWriter(f, fieldnames=["precision", "recall", "cpu_time", "total_completion_time"])
+        f = open("./marc_experiments/exp_2_hyperSets_2_" + model + ".csv", 'w')
+        writer = csv.DictWriter(f, fieldnames=["set_index", "precision", "recall", "cpu_time", "total_completion_time"])
         writer.writeheader()
         writer.writerows(parsed_results)
         f.close()
--- fasttext_pywren/HyperParametersEvaluate/randomHyperparameters.py
+++ fasttext_pywren/HyperParametersEvaluate/randomHyperparameters.py
@@ -1,23 +1,19 @@
 from random import choice
 import numpy as np
 
-list_size = 50
-
 lr_values = np.arange(0.1, 1.1, 0.1)
-
 lrUpdateRate_values = np.arange(20, 110, 10)
-
 ws_values = np.arange(3, 8, 1)
-
 epoch_values = np.array([1, 5, 10])
 
-iter_hyperparameters = list()
-for i in range(list_size):
-    hyperparameters_set = {
-        "lr": choice(lr_values),
-        "lrUpdateRate": choice(lrUpdateRate_values),
-        "ws": choice(ws_values),
-        "epoch": choice(epoch_values)}
-    iter_hyperparameters.append([hyperparameters_set])
 
-print(iter_hyperparameters)
+def random_search(sets_num: int = 10):
+    iter_hyperparameters = []
+    for i in range(sets_num):
+        hyperparameters_set = {
+            "lr": choice(lr_values),
+            "lrUpdateRate": choice(lrUpdateRate_values),
+            "ws": choice(ws_values),
+            "epoch": choice(epoch_values)}
+        iter_hyperparameters.append([hyperparameters_set])
+    return iter_hyperparameters
