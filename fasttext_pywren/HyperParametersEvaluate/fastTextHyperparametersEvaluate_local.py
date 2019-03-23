b260b59b6ea8da242b166ba17868ed010560aeb7
idoye <idoye@campus.technion.ac.il>
2019-03-23 20:56:43 +0200

    ex1 csv files

--- fasttext_pywren/HyperParametersEvaluate/fastTextHyperparametersEvaluate_local.py
+++ fasttext_pywren/HyperParametersEvaluate/fastTextHyperparametersEvaluate_local.py
@@ -1,6 +1,6 @@
 import fastText as fstTxt
-from localGenericHyperparameterEvaluate import LocalKFoldCrossValidation
-from randomHyperparameters import random_search
+from fasttext_pywren.HyperParametersEvaluate.localGenericHyperparameterEvaluate import *
+from fasttext_pywren.HyperParametersEvaluate.randomHyperparameters import random_search
 from time import time
 import csv
 
@@ -29,49 +29,49 @@ import csv
 #
 # yelp_folds.k_fold_cross_partitioner()
 
-def fastText_evaluate(train_path, test_path, hyperparameters_set):
+def fastText_evaluate(queue, train_path, test_path, hyperparameters_set):
     start = time()
     to_valid_model = fstTxt.train_supervised(train_path, **hyperparameters_set)
     result = to_valid_model.test(test_path)
     end = time()
-    return {"precision": result[1], "recall": result[2], "cpu_time": end - start}
+    queue.put({"precision": result[1], "recall": result[2], "cpu_time": end - start})
 
 
 if __name__ == '__main__':
-    # """experiments 1 default hyperparameters"""
-    # for model in ["ag_news", "dbpedia", "yelp"]:
-    #     results = []
-    #     for i in range(10):
-    #         lkf = LocalKFoldCrossValidation(5, model, ("precision", "recall", "cpu_time"), fastText_evaluate)
-    #         results.append(lkf.hyperparameters_kfc_parallel())
-    #
-    #     parsed_results = []
-    #     for iteration_result in results:
-    #         parsed_results.append({**iteration_result["results"][0], 'total_completion_time': iteration_result['total_completion_time']})
-    #
-    #     f = open("./marc_experiments/exp_1_" + model + ".csv", 'w')
-    #     writer = csv.DictWriter(f, fieldnames=["precision", "recall", "cpu_time", "total_completion_time"])
-    #     writer.writeheader()
-    #     writer.writerows(parsed_results)
-    #     f.close()
+    """experiments 1 default hyperparameters"""
+    for model in ["ag_news", "dbpedia", "yelp"]:
+        results = []
+        for i in range(10):
+            lkf = LocalKFoldCrossValidation(5, model, ("precision", "recall", "cpu_time"), fastText_evaluate)
+            results.append(lkf.hyperparameters_kfc_parallel())
+
+        parsed_results = []
+        for iteration_result in results:
+            parsed_results.append({**iteration_result["results"][0], 'total_completion_time': iteration_result['total_completion_time']})
+
+        f = open("./marc_experiments/exp_1_" + model + ".csv", 'w')
+        writer = csv.DictWriter(f, fieldnames=["precision", "recall", "cpu_time", "total_completion_time"])
+        writer.writeheader()
+        writer.writerows(parsed_results)
+        f.close()
 
     """experiments 2 random search hyperparameters"""
-    for number_of_sets in [10]:
-        hyperparameters_sets = random_search(number_of_sets)
-        for model in ["ag_news"]:# , "dbpedia", "yelp"]:
-            results = []
-            for i in range(5):
-                lkf = LocalKFoldCrossValidation(5, model, ("precision", "recall", "cpu_time"), fastText_evaluate)
-                results.append(lkf.hyperparameters_kfc_parallel(hyperparameters_sets))
-
-            parsed_results = []
-            for iteration_result in results:
-                for set_index, set_res in enumerate(iteration_result["results"]):
-                    parsed_results.append(
-                        {"set_index": set_index, **set_res, 'total_completion_time': iteration_result['total_completion_time']})
-
-            f = open("./marc_experiments/exp_2_hyperSets_" + str(number_of_sets) + "_" + model + ".csv", 'w')
-            writer = csv.DictWriter(f, fieldnames=["set_index", "precision", "recall", "cpu_time", "total_completion_time"])
-            writer.writeheader()
-            writer.writerows(parsed_results)
-            f.close()
+    # for number_of_sets in [20]:
+    #     hyperparameters_sets = random_search(number_of_sets)
+    #     for model in ["ag_news"]:# , "dbpedia", "yelp"]:
+    #         results = []
+    #         for i in range(5):
+    #             lkf = LocalKFoldCrossValidation(5, model, ("precision", "recall", "cpu_time"), fastText_evaluate)
+    #             results.append(lkf.hyperparameters_kfc_parallel(hyperparameters_sets))
+    #
+    #         parsed_results = []
+    #         for iteration_result in results:
+    #             for set_index, set_res in enumerate(iteration_result["results"]):
+    #                 parsed_results.append(
+    #                     {"set_index": set_index, **set_res, 'total_completion_time': iteration_result['total_completion_time']})
+    #
+    #         f = open("./marc_experiments/exp_2_hyperSets_" + str(number_of_sets) + "_" + model + ".csv", 'w')
+    #         writer = csv.DictWriter(f, fieldnames=["set_index", "precision", "recall", "cpu_time", "total_completion_time"])
+    #         writer.writeheader()
+    #         writer.writerows(parsed_results)
+    #         f.close()
--- fasttext_pywren/HyperParametersEvaluate/localGenericHyperparameterEvaluate.py
+++ fasttext_pywren/HyperParametersEvaluate/localGenericHyperparameterEvaluate.py
@@ -1,4 +1,5 @@
-import billiard as multiprocessing
+import multiprocessing as mp
+
 from time import time
 
 
@@ -16,23 +17,32 @@ def _reducer_average_validator(evaluate_keys, results):
     return k_cross_valid_dict
 
 
-def _evaluate_single_hyperparameters_parallel(k_value,
+def _evaluate_single_hyperparameters_parallel(queue, k_value,
                                               folder_path,
                                               model_name,
                                               evaluate_function,
                                               evaluate_keys,
                                               hyperparameters_set={}):
     args_list = []
+    sub_q = mp.Queue()
+
     for eval_index in range(k_value):
         path: str = folder_path
         train_path: str = path + model_name + "_fold_" + str(eval_index) + ".train"
         test_path: str = path + model_name + "_fold_" + str(eval_index) + ".test"
-        args_list.append((train_path, test_path, hyperparameters_set))
+        args_list.append((sub_q, train_path, test_path, hyperparameters_set))
 
-    p = multiprocessing.Pool(k_value)
-    results = p.starmap(func=evaluate_function, iterable=args_list)
+    processes = [mp.Process(target=evaluate_function, args=args_list[i]) for i in range(k_value)]
+
+    for p in processes:
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    results = [sub_q.get() for p in processes]
     results = _reducer_average_validator(evaluate_keys, results)
-    return results
+    queue.put(results)
 
 
 class LocalKFoldCrossValidation(object):
@@ -67,15 +77,23 @@ class LocalKFoldCrossValidation(object):
     def hyperparameters_kfc_parallel(self, hyperparameters_sets=[[{}]]):
         start_time = time()
         args_list = []
+        q = mp.Queue()
         for hyp_set in hyperparameters_sets:
-            args_list.append((self.k_value,
+            args_list.append((q, self.k_value,
                               self.folder_path,
                               self.model_name,
                               self.evaluate_function,
                               self.evaluate_keys,
                               hyp_set[0]))
 
-        p = multiprocessing.Pool(len(hyperparameters_sets))
-        results = p.starmap(func=_evaluate_single_hyperparameters_parallel, iterable=args_list)
+        processes = [mp.Process(target=_evaluate_single_hyperparameters_parallel, args=args_list[i]) for i in range(len(hyperparameters_sets))]
+
+        for p in processes:
+            p.start()
+
+        for p in processes:
+            p.join()
+
+        results = [q.get() for p in processes]
         total_completion_time = time() - start_time
         return {"results": results, "total_completion_time": total_completion_time}
