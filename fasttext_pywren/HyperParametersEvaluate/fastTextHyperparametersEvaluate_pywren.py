5c5d658fe74677d83c0b561381e27222a5a436a1
idoye <idoye@campus.technion.ac.il>
2019-04-05 16:32:06 +0300

    exp1 update stat

--- fasttext_pywren/HyperParametersEvaluate/fastTextHyperparametersEvaluate_pywren.py
+++ fasttext_pywren/HyperParametersEvaluate/fastTextHyperparametersEvaluate_pywren.py
@@ -19,7 +19,7 @@ def fastText_evaluate(train_path, test_path, hyperparameters_set):
 
 if __name__ == '__main__':
     """experiments 1 default hyperparameters"""
-    for model in ["ag_news", "dbpedia", "yelp"]:
+    for model in ["ag_news", "dbpedia"]:  # , "yelp"]: causing out of memory
         results = []
         for i in range(10):
             """call for PyWren exaction with list of hyperparameters"""
@@ -33,6 +33,7 @@ if __name__ == '__main__':
             pywren_kcfv.set_kvalue(5)
             pywren_kcfv.set_evaluation_keys(("precision", "recall", "cpu_time"))
             pywren_kcfv.set_parameters([[{}]])
+            print("model is: " + model + ", iteration #: " + str(i))
             results.append(pywren_kcfv.evaluate_params(runtime="idoye/fasttext-hyperparameters_3.7"))
         parsed_results = []
         for iteration_result in results:
@@ -45,9 +46,9 @@ if __name__ == '__main__':
         f.close()
 
     """experiments 2 random search hyperparameters pywren"""
-    for number_of_sets in [1, 5, 25, 50, 75]:  # , 10, 20, 40, 80]:
+    for number_of_sets in [5, 25, 50, 75]:
         hyperparameters_sets = random_search(number_of_sets)
-        for model in ["ag_news", "dbpedia", "yelp"]:
+        for model in ["ag_news", "dbpedia"]:  # , "yelp"]: causing out of memory
             results = []
             for i in range(5):
                 """call for PyWren exaction with list of hyperparameters"""
@@ -61,6 +62,7 @@ if __name__ == '__main__':
                 pywren_kcfv.set_kvalue(5)
                 pywren_kcfv.set_evaluation_keys(("precision", "recall", "cpu_time"))
                 pywren_kcfv.set_parameters(hyperparameters_sets)
+                print("model is: " + model + " # sets is: " + str(number_of_sets) + ", iteration #: " + str(i))
                 results.append(pywren_kcfv.evaluate_params(runtime="idoye/fasttext-hyperparameters"))
 
             parsed_results = []
@@ -69,7 +71,7 @@ if __name__ == '__main__':
                     parsed_results.append(
                         {"set_index": set_index, **set_res, 'total_completion_time': iteration_result['total_completion_time']})
 
-            f = open("./marc_experiments/exp_2_hyperSets_" + str(number_of_sets) + "_" + model + ".csv", 'w')
+            f = open("./marc_experiments/pywren_exp_2_hyperSets_" + str(number_of_sets) + "_" + model + ".csv", 'w')
             writer = csv.DictWriter(f, fieldnames=["set_index", "precision", "recall", "cpu_time", "total_completion_time"])
             writer.writeheader()
             writer.writerows(parsed_results)
